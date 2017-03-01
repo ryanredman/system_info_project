25910e20253d62ec747ab06db574ba4c5f0a362b
Ryan Redman <rmr3712@gmail.com>
2017-03-01 13:34:21 -0800

    Altered system_info go project so that the switches primarily control properly formating output.
    Additionally, available URL endpoints can be created in the global slice
    which will allow for the reading of any proc file.

--- system_info/src/main.go
+++ system_info/src/main.go
@@ -7,43 +7,71 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-	"regexp"
+	"strings"
 )
 
+var endpoints = []string{"cpuinfo", "meminfo", "uptime"}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path[1:] {
-	case "cpuinfo":
-		fallthrough
-	case "meminfo":
-		fallthrough
-	case "uptime":
-		out, err := exec.Command("cat", "/proc/"+r.URL.Path[1:]).Output()
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		out_reader := bytes.NewReader(out)
+	target := r.URL.Path[1:]
 
-		scanner := bufio.NewScanner(out_reader)
-		for scanner.Scan() {
-			fmt.Fprintln(w, re_redun_whtsp.ReplaceAllString(scanner.Text(), " "))
-		}
+	for _, e := range endpoints {
+		if target == e {
+			out, err := exec.Command("cat", "/proc/"+r.URL.Path[1:]).Output()
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
+			out_hash := make(map[string]string)
+			out_reader := bytes.NewReader(out)
+			scanner := bufio.NewScanner(out_reader)
+			for scanner.Scan() {
+				var kv_arr []string
+				line := scanner.Text()
+
+				switch target {
+				case "cpuinfo":
+					line = strings.Replace(line, "  ", "", -1)
+					line = strings.Replace(line, "\t", "", -1)
+					line = strings.Replace(line, ": ", ":", -1)
+					kv_arr = strings.Split(line, ":")
+
+					if len(kv_arr) > 1 {
+						out_hash[kv_arr[0]] = kv_arr[1]
+					} else {
+						out_hash[kv_arr[0]] = ""
+					}
+				case "meminfo":
+					line = strings.Replace(line, "  ", "", -1)
+					line = strings.Replace(line, ": ", ":", -1)
+					kv_arr = strings.Split(line, ":")
+
+					if len(kv_arr) > 1 {
+						out_hash[kv_arr[0]] = kv_arr[1]
+					} else {
+						out_hash[kv_arr[0]] = ""
+					}
+				case "uptime":
+					out_hash["uptime"] = strings.Split(line, " ")[0]
+					out_hash["idle"] = strings.Split(line, " ")[1]
+				}
+
+				if err := scanner.Err(); err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			fmt.Fprintln(w, out_hash)
+			return
 		}
-	default:
-		//TODO - small html page with links to endpoints
-		fmt.Fprintln(w, "Endpoints: cpuinfo, meminfo, uptime")
 	}
+
+	fmt.Fprintln(w, "Endpoints: cpuinfo, meminfo, uptime")
+	return
 }
 
-var re_redun_whtsp *regexp.Regexp
-
 func main() {
-
-	re_redun_whtsp = regexp.MustCompile(`[\s\p{Zs}]{2,}`)
-
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":1404", nil)
 }
