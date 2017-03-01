46bedcd425ac4529e57a92dd49b2ca6442de553c
Ryan Redman <rmr3712@gmail.com>
2017-03-01 12:36:53 -0800

    Simplified Go code to only have one endpoint function, and simply switch on valid endpoints.

--- system_info/src/main.go
+++ system_info/src/main.go
@@ -8,67 +8,33 @@ import (
 	"net/http"
 	"os/exec"
 	"regexp"
-	//"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	//TODO - small html page with links to endpoints
-	fmt.Fprintln(w, "Get: cpuinfo, meminfo, uptime")
-}
+	switch r.URL.Path[1:] {
+	case "cpuinfo":
+		fallthrough
+	case "meminfo":
+		fallthrough
+	case "uptime":
+		out, err := exec.Command("cat", "/proc/"+r.URL.Path[1:]).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-func mem_handler(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("cat", "/proc/meminfo").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
+		out_reader := bytes.NewReader(out)
 
-	out_reader := bytes.NewReader(out)
+		scanner := bufio.NewScanner(out_reader)
+		for scanner.Scan() {
+			fmt.Fprintln(w, re_redun_whtsp.ReplaceAllString(scanner.Text(), " "))
+		}
 
-	scanner := bufio.NewScanner(out_reader)
-	for scanner.Scan() {
-		fmt.Fprintln(w, re_redun_whtsp.ReplaceAllString(scanner.Text(), " "))
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func cpu_handler(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("cat", "/proc/cpuinfo").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out_reader := bytes.NewReader(out)
-
-	scanner := bufio.NewScanner(out_reader)
-	for scanner.Scan() {
-		//fmt.Fprintln(w, re_redun_whtsp.ReplaceAllString(scanner.Text(), " "))
-		fmt.Fprintln(w, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func upt_handler(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("cat", "/proc/uptime").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out_reader := bytes.NewReader(out)
-
-	scanner := bufio.NewScanner(out_reader)
-	for scanner.Scan() {
-		fmt.Fprintln(w, re_redun_whtsp.ReplaceAllString(scanner.Text(), " "))
-		//fmt.Fprintln(w, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		//TODO - small html page with links to endpoints
+		fmt.Fprintln(w, "Endpoints: cpuinfo, meminfo, uptime")
 	}
 }
 
@@ -79,8 +45,5 @@ func main() {
 	re_redun_whtsp = regexp.MustCompile(`[\s\p{Zs}]{2,}`)
 
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/cpuinfo", cpu_handler)
-	http.HandleFunc("/meminfo", mem_handler)
-	http.HandleFunc("/uptime", upt_handler)
 	http.ListenAndServe(":1404", nil)
 }
